994a9ac7ef6dd575c9acb244e5406844ae82f3e6
Blake Walsh <blakeofthepath@gmail.com>
2018-12-31 13:22:33 +0200

    OAuth app stuff is now optional, no setup is required for development

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, redirect, url_for, session, request, jsonify, render_template_string
 from flask_oauthlib.client import OAuth
 from flask_cors import CORS
@@ -8,25 +9,14 @@ from github import Github
 
 from config import config
 
+
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['JSON_AS_ASCII'] = False
 app.config['JSON_SORT_KEYS'] = False
 app.debug = True
 app.secret_key = 'development'
-oauth = OAuth(app)
-
-github_auth = oauth.remote_app(
-    'github',
-    consumer_key=config.GIT_APP_KEY,
-    consumer_secret=config.GIT_APP_SECRET,
-    request_token_params={'scope': 'repo read:user'},
-    base_url='https://api.github.com/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',
-    authorize_url='https://github.com/login/oauth/authorize'
-)
 
 @app.route('/api/segments/', methods=['GET'])
 def segments():
@@ -48,56 +38,73 @@ def nav():
     import fs
     return jsonify(fs.get_condensed_tree(['translation']))
 
-@app.route('/')
-def index():
-    if 'github_token' in session:
-        data = github_auth.get('user').data
-        return jsonify(call_github())
-    return render_template_string('<a href="{{ url_for("login") }}">Login</a>')
+try:
+    oauth = OAuth(app)
+
+    github_auth = oauth.remote_app(
+        'github',
+        consumer_key=config.GIT_APP_KEY,
+        consumer_secret=config.GIT_APP_SECRET,
+        request_token_params={'scope': 'repo read:user'},
+        base_url='https://api.github.com/',
+        request_token_url=None,
+        access_token_method='POST',
+        access_token_url='https://github.com/login/oauth/access_token',
+        authorize_url='https://github.com/login/oauth/authorize'
+    )
+
+    @app.route('/')
+    def index():
+        if 'github_token' in session:
+            data = github_auth.get('user').data
+            return jsonify(call_github())
+        return render_template_string('<a href="{{ url_for("login") }}">Login</a>')
 
 
 
-@app.route('/login')
-def login():
-    return github_auth.authorize(callback=url_for('authorized', _external=True))
+    @app.route('/login')
+    def login():
+        return github_auth.authorize(callback=url_for('authorized', _external=True))
 
 
-@app.route('/logout')
-def logout():
-    session.pop('github_token', None)
-    return redirect(url_for('index'))
+    @app.route('/logout')
+    def logout():
+        session.pop('github_token', None)
+        return redirect(url_for('index'))
 
 
-@app.route('/authorized')
-def authorized():
-    resp = github_auth.authorized_response()
-    if resp is None or resp.get('access_token') is None:
-        return 'Access denied: reason=%s error=%s resp=%s' % (
-            request.args['error'],
-            request.args['error_description'],
-            resp
-        )
-    session['github_token'] = (resp['access_token'], '')
-    me = github_auth.get('user')
-    return jsonify(me.data)
+    @app.route('/authorized')
+    def authorized():
+        resp = github_auth.authorized_response()
+        if resp is None or resp.get('access_token') is None:
+            return 'Access denied: reason=%s error=%s resp=%s' % (
+                request.args['error'],
+                request.args['error_description'],
+                resp
+            )
+        session['github_token'] = (resp['access_token'], '')
+        me = github_auth.get('user')
+        return jsonify(me.data)
 
 
-@github_auth.tokengetter
-def get_github_oauth_token():
-    return session.get('github_token')
+    @github_auth.tokengetter
+    def get_github_oauth_token():
+        return session.get('github_token')
 
 
-def call_github():
-    github = Github(session.get('github_token')[0])
-    result = []
-    for repo in github.get_user().get_repos():
-        result.append(repo.name)
-    return result
+    def call_github():
+        github = Github(session.get('github_token')[0])
+        result = []
+        for repo in github.get_user().get_repos():
+            result.append(repo.name)
+        return result
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    return 'Okay', 200
+    @app.route('/webhook', methods=['POST'])
+    def webhook():
+        data = request.get_json()
+        return 'Okay', 200
+except Exception as e:
+    logging.error(f'An error occured while setting up OAuth app {str(e)}')
 
 if __name__ == '__main__':
     app.run()
