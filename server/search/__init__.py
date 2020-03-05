39fad43babc83ae73aaf3eb13ab8db94031b6340
Blake Walsh <blakeofthepath@gmail.com>
2020-03-05 11:33:56 +0200

    TM/search is now optional

--- server/search/__init__.py
+++ server/search/__init__.py
@@ -1,3 +1,8 @@
-from .arango_search import Search
+import logging
+try:
+    from .arango_search import Search
 
-search = Search()
+    search = Search()
+except Exception as e:
+    logging.exception("Could not initiate search")
+    search = None
