052f47341fa389b92acf07c8c0b60d0afbfb82ac
Blake Walsh <blakeofthepath@gmail.com>
2020-04-14 19:19:45 +0300

    Fix issue where URLs with . in them return 404 error in development.

--- client/es-dev-server.config.js
+++ client/es-dev-server.config.js
@@ -7,9 +7,14 @@ module.exports = {
 	appIndex: 'index.html',
 	moduleDirs: ['node_modules'],
 	middlewares: [
+    function dotFix(context, next) {
+      if (!context.url.match(/\/api/) && context.url.match(/\.\d/)) {
+        context.url = '/index.html';
+      }
+      return next();
+    },
 		proxy('/api', {
-			target: 'http://localhost:5000',
-			logs: true
+			target: 'http://localhost:5000'
 		})
 	]
 }
