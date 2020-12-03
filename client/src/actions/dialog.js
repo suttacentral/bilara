cf342857e036e0639f59d9d06a46eb99b857463a
Blake Walsh <blakeofthepath@gmail.com>
2020-12-03 15:14:49 +0200

    Added dialog.js which somehow got missed

---
+++ client/src/actions/dialog.js
@@ -0,0 +1,22 @@
+export const SET_PUBLISH_DATA = 'SET_PUBLISH_DATA';
+export const RESET_DIALOG_DATA = 'RESET_DIALOG_DATA';
+export const CLOSE_DIALOG = 'CLOSE_DIALOG';
+
+export const setPublishData = (data, reset) => dispatch => {
+  if (reset) {
+    dispatch({
+      type: RESET_DIALOG_DATA
+    })
+  }
+  
+  dispatch({
+    type: SET_PUBLISH_DATA,
+    data: data
+  })
+}
+
+export const closeDialog = () => dispatch => {
+  dispatch({
+    type: CLOSE_DIALOG
+  })
+}
