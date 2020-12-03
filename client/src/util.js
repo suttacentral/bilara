23e23b90b4f45e91bf0c3ddcb2c76617d38371b6
Blake Walsh <blakeofthepath@gmail.com>
2020-12-03 15:22:45 +0200

    publish feature explicitly enabled

--- client/src/util.js
+++ client/src/util.js
@@ -37,7 +37,7 @@ export const contentEditableValue = (()=>{
 
 export const featureFlags = {
   search: true, //!!window.location.host.match(/localhost\b/)
-  publish: !!window.location.host.match(/localhost\b/),
+  publish: true,
 }
 
 console.log(featureFlags);
