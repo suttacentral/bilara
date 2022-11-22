4bcfd65944bd94b22f5386e75e7b9a97f69573ca
Blake Walsh <blakeofthepath@gmail.com>
2022-11-22 13:17:00 +0100

    Attempt to fix problem with service worker by making it less aggressive

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -130,11 +130,7 @@ export default {
       // where to output the generated sw
       swDest: path.join('dist', 'sw.js'),
       // directory to match patterns against to be precached
-      globDirectory: path.join('dist'),
-      // cache any html js and css by default
-      globPatterns: ['**/*.{html,js,css,webmanifest}'],
-      skipWaiting: true,
-      clientsClaim: true,
+      globDirectory: path.join('dist')
     }),
   ],
 };
